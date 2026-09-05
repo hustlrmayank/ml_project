31f53f3f9ea434cc245f76bd712ca056433568ed
Mayank sharma <hustlrmayank001@gmail.com>
2026-09-05 11:02:18 +0530

    data ingestion and transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 from dataclasses import dataclass
 
 @dataclass
@@ -31,7 +33,8 @@ class DataIngestion:
             
             train_set,test_set = train_test_split(df,test_size=0.2, random_state=42)
             
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=False)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             
             logging.info("Ingestion of the data iss completed")
             
@@ -45,8 +48,13 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()    
-        
+    train_data,test_data =obj.initiate_data_ingestion() 
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+       
+      
+       
             
             
              
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 import numpy as np 
 import pandas as pd
 import dill
@@ -13,7 +12,6 @@ from src.exception import CustomException
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
